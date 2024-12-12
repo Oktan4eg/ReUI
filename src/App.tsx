d65aaeb7040acb3ebc38a9fbf9dcbb6ff9710f1a
Sasha <144614461+Oktan4eg@users.noreply.github.com>
2024-12-12 20:32:01 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,44 @@
-import {
-  ShowTokens,
-  // ShowTokens2,
-  Link,
-  Text,
-  Radio,
-  Checkbox,
-  GlobalStyle,
-  lightTheme,
-  darkTheme,
-  Button,
-  Toggle,
-} from './lib';
+import { Text, GlobalStyle, lightTheme, darkTheme } from './lib';
 import { useTheme } from './lib/hooks';
 import { ThemeProvider } from 'styled-components';
 import { palette, sizes, font } from './lib/theme/tokensMY';
+import { EducationLogo } from './lib/components/for-education/education-logo';
+// import Link from "next/link";
 
 function App() {
   const { theme, setTheme } = useTheme();
 
   return (
-    <main style={{ display: 'grid', padding: '96px', gap: '96px' }}>
+    <main style={{ display: 'flex', padding: '96px', gap: '96px' }}>
       <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
         <GlobalStyle />
+        {/* <EducationLogo /> */}
+        <nav>
+          <a href='/'>
+            <EducationLogo />
+          </a>
+          {/* <link /> */}
+          <a href='/programmms'>
+            <Text size={'label'} scale='lg' children={'Программы'} />
+          </a>
+          <a href='/franchise'>
+            <Text size={'label'} scale='lg' children={'Франшиза'} />
+          </a>
+          <a href='/faq'>
+            <Text size={'label'} scale='lg' children={'Вопросы и ответы'} />
+          </a>
+          <a href='/contacts'>
+            <Text size={'label'} scale='lg' children={'Контакты'} />
+          </a>
+          <a href='/courses'>
+            <Text size={'label'} scale='lg' children={'Мои курсы'} />
+          </a>
+          {/* <Text size={'label'} scale='lg' children={'Программы'} />
+          <Text size={'label'} scale='lg' children={'Франшиза'} />
+          <Text size={'label'} scale='lg' children={'Вопросы и ответы'} />
+          <Text size={'label'} scale='lg' children={'Контакты'} />
+          <Text size={'label'} scale='lg' children={'Мои курсы'} /> */}
+        </nav>
       </ThemeProvider>
     </main>
   );
